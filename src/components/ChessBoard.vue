37da9980edcb4d3c8e245c2b6c65ca0e73d3c604
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-08-29 22:20:36 +0200

    Handling knight drag and drop

--- src/components/ChessBoard.vue
+++ src/components/ChessBoard.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="grid">
+  <div
+    ref="rootElt"
+    class="grid"
+    @mousedown="handleDragStart"
+    @mousemove="handleDrag"
+    @mouseup="handleDragEnd"
+  >
     <div
       class="grid_row"
       v-for="row in [0, 1, 2, 3, 4, 5, 6, 7]"
@@ -9,22 +15,33 @@
         :class="classForCell(row, col)"
         v-for="col in [0, 1, 2, 3, 4, 5, 6, 7]"
         :key="'cell_' + row + col"
-      >
-        <img
-          v-if="isPlayerKnightPos(row, col)"
-          src="@/assets/chess_vectors/Chess_nlt45.svg"
-          width="50"
-          height="50"
-        />
-      </div>
+      ></div>
+    </div>
+    <div class="pieces_layer">
+      <img
+        id="player_knight"
+        src="@/assets/chess_vectors/Chess_nlt45.svg"
+        :style="{
+          left: playerKnightLeft,
+          top: playerKnightTop,
+        }"
+        width="50"
+        height="50"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
+
+const cellSizePx = 50;
+
 export default {
   setup() {
+    const rootElt = ref();
+    const dndData = ref();
+
     const playerKnightPos = ref({
       col: 3,
       row: 5,
@@ -37,15 +54,108 @@ export default {
     }
 
     function isPlayerKnightPos(row, col) {
-        console.log(row === playerKnightPos.value.row && col === playerKnightPos.value.col);
       return (
         row === playerKnightPos.value.row && col === playerKnightPos.value.col
       );
     }
 
+    function handleDragStart(event) {
+      const { clientX, clientY } = event;
+      const rootElementRect = rootElt.value.getBoundingClientRect();
+      const rootEltX = rootElementRect.left;
+      const rootEltY = rootElementRect.top;
+      const evtX = clientX - rootEltX;
+      const evtY = clientY - rootEltY;
+
+      const evtCol = parseInt(Math.floor(evtX / cellSizePx));
+      const evtRow = parseInt(Math.floor(evtY / cellSizePx));
+
+      const isPlayerKnightCell =
+        evtCol === playerKnightPos.value.col &&
+        evtRow === playerKnightPos.value.row;
+
+      if (!isPlayerKnightCell) return;
+      dndData.value = {
+        left: evtCol * cellSizePx,
+        top: evtRow * cellSizePx,
+      };
+    }
+
+    function handleDrag(event) {
+      if (!dndData.value) return;
+
+      const { clientX, clientY } = event;
+      const rootElementRect = rootElt.value.getBoundingClientRect();
+      const rootEltX = rootElementRect.left;
+      const rootEltY = rootElementRect.top;
+      const evtX = clientX - rootEltX;
+      const evtY = clientY - rootEltY;
+
+      dndData.value = {
+        left: evtX,
+        top: evtY,
+      };
+    }
+
+    function handleDragEnd(event) {
+      if (!dndData.value) return;
+
+      const { clientX, clientY } = event;
+      const rootElementRect = rootElt.value.getBoundingClientRect();
+      const rootEltX = rootElementRect.left;
+      const rootEltY = rootElementRect.top;
+      const evtX = clientX - rootEltX;
+      const evtY = clientY - rootEltY;
+
+      const evtCol = parseInt(Math.floor(evtX / cellSizePx));
+      const evtRow = parseInt(Math.floor(evtY / cellSizePx));
+
+      const colInBounds = evtCol >= 0 && evtCol <= 7;
+      const rowInBounds = evtRow >= 0 && evtRow <= 7;
+
+      if (colInBounds && rowInBounds) {
+        const absDeltaCol = Math.abs(playerKnightPos.value.col - evtCol);
+        const absDeltaRow = Math.abs(playerKnightPos.value.row - evtRow);
+
+        const validMove =
+          absDeltaCol > 0 && absDeltaRow > 0 && absDeltaRow + absDeltaCol === 3;
+        if (validMove) {
+          playerKnightPos.value = {
+            col: evtCol,
+            row: evtRow,
+          };
+        }
+      }
+
+      dndData.value = undefined;
+    }
+
+    const dndIsActive = computed(() => !!dndData.value);
+
+    const playerKnightLeft = computed(() => {
+      const newValue = dndData.value
+        ? dndData.value.left
+        : playerKnightPos.value.col * cellSizePx;
+      return newValue + "px";
+    });
+    const playerKnightTop = computed(() => {
+      const newValue = dndData.value
+        ? dndData.value.top
+        : playerKnightPos.value.row * cellSizePx;
+      return newValue + "px";
+    });
+
     return {
+      rootElt,
       isPlayerKnightPos,
       classForCell,
+      handleDragStart,
+      handleDrag,
+      handleDragEnd,
+      dndIsActive,
+      dndData,
+      playerKnightLeft,
+      playerKnightTop,
     };
   },
 };
@@ -53,6 +163,7 @@ export default {
 
 <style scoped>
 .grid {
+  position: relative;
   width: 400px;
   height: 400px;
   display: grid;
@@ -70,5 +181,15 @@ export default {
 
 .black_cell {
   background-color: peru;
+}
+
+.pieces_layer {
+  position: absolute;
+  width: 400px;
+  height: 400px;
+}
+
+#player_knight {
+  position: absolute;
 }
 </style>
