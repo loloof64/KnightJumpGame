fab569aa7021ea13806a4f05565b642e9387cc98
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-08-30 13:33:02 +0200

    Little fix for the translations system.

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,7 +2,7 @@ import en from "./en";
 import es from "./es";
 import fr from "./fr";
 
-const locale = navigator.language || navigator.languages[0];
+const locale = (navigator.language || navigator.languages[0]).substring(0, 2);
 
 const messages = {
   en,
